b178d5b44cc53a97e143ad424041e7b59de4b511
JoyceeOliveira <87099853+JoyceeOliveira@users.noreply.github.com>
2021-07-08 16:12:18 -0300

    Add files via upload

---
+++ Questão 5 F.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+  
+  // ?????
+  
+  
+  
+int main(){
+		 
+  char aux;
+  char nome[] = "Ponteiros" ;
+  char *p3;		
+  
+  /* (f) */
+  p3 = p3 + 4;
+  printf("%c \n", *p3);
+	
+	return 0;
+}
+
+
---
+++ Questão 5 G.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+// ?????
+
+int main(){
+
+  char aux;
+  char nome[] = "Ponteiros" ;
+  char *p3;		
+
+  /* (g) */
+  
+  p3--;
+  printf("%c \n", *p3);
+  
+  return 0;
+  
+}
